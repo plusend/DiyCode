e5f052e4e8a5de4c585005f4afdcd03d8b68a43c
jian.zhang <jian.zhang@yulore.com>
2017-02-16 20:04:20 +0800

    更新大图预览

--- app/src/main/java/com/plusend/diycode/view/activity/ImageActivity.java
+++ app/src/main/java/com/plusend/diycode/view/activity/ImageActivity.java
@@ -8,11 +8,8 @@ import android.util.Log;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.backends.pipeline.PipelineDraweeControllerBuilder;
 import com.facebook.drawee.controller.BaseControllerListener;
-import com.facebook.drawee.drawable.ProgressBarDrawable;
-import com.facebook.drawee.generic.GenericDraweeHierarchy;
-import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
+import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.imagepipeline.image.ImageInfo;
 import com.plusend.diycode.R;
 import com.plusend.diycode.mvp.model.base.Presenter;
@@ -34,28 +31,21 @@ public class ImageActivity extends BaseActivity {
     String url = getIntent().getStringExtra(URL);
     Log.d(TAG, "url: " + url);
 
-    GenericDraweeHierarchyBuilder builder = new GenericDraweeHierarchyBuilder(getResources());
-    GenericDraweeHierarchy hierarchy =
-        builder.setFadeDuration(300).setProgressBarImage(new ProgressBarDrawable()).build();
-    mPhotoDraweeView.setHierarchy(hierarchy);
-
-    // 需要使用 ControllerBuilder 方式请求图片
-    PipelineDraweeControllerBuilder controller = Fresco.newDraweeControllerBuilder();
-    controller.setUri(Uri.parse(url));
-    controller.setOldController(mPhotoDraweeView.getController());
-    controller.setAutoPlayAnimations(true);
-
-    // 需要设置 ControllerListener，获取图片大小后，传递给 PhotoDraweeView 更新图片长宽
-    controller.setControllerListener(new BaseControllerListener<ImageInfo>() {
-      @Override public void onFinalImageSet(String id, ImageInfo imageInfo, Animatable animatable) {
-        super.onFinalImageSet(id, imageInfo, animatable);
-        if (imageInfo == null || mPhotoDraweeView == null) {
-          return;
-        }
-        mPhotoDraweeView.update(imageInfo.getWidth(), imageInfo.getHeight());
-      }
-    });
-    mPhotoDraweeView.setController(controller.build());
+    DraweeController controller = Fresco.newDraweeControllerBuilder()
+        .setUri(Uri.parse(url))
+        .setAutoPlayAnimations(true)
+        .setControllerListener(new BaseControllerListener<ImageInfo>() {
+          @Override
+          public void onFinalImageSet(String id, ImageInfo imageInfo, Animatable animatable) {
+            super.onFinalImageSet(id, imageInfo, animatable);
+            if (imageInfo == null || mPhotoDraweeView == null) {
+              return;
+            }
+            mPhotoDraweeView.update(imageInfo.getWidth(), imageInfo.getHeight());
+          }
+        })
+        .build();
+    mPhotoDraweeView.setController(controller);
   }
 
   @Override protected Toolbar getToolbar() {
