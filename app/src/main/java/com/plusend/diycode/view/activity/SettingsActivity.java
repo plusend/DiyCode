96fe70c86059e866e14783185ff8ceda8068f23e
jian.zhang <jian.zhang@yulore.com>
2017-02-17 10:21:17 +0800

    SettingActivity 滑动返回

--- app/src/main/java/com/plusend/diycode/view/activity/SettingsActivity.java
+++ app/src/main/java/com/plusend/diycode/view/activity/SettingsActivity.java
@@ -1,43 +1,35 @@
 package com.plusend.diycode.view.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 import android.widget.FrameLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.plusend.diycode.R;
+import com.plusend.diycode.mvp.model.base.Presenter;
 import com.plusend.diycode.view.fragment.SettingsFragment;
+import java.util.List;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends BaseActivity {
   private static final String TAG = "SettingsActivity";
   @BindView(R.id.container) FrameLayout container;
+  @BindView(R.id.toolbar) Toolbar toolbar;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_settings);
     ButterKnife.bind(this);
-    setActionBar();
+    super.onCreate(savedInstanceState);
 
     getFragmentManager().beginTransaction()
         .replace(R.id.container, new SettingsFragment())
         .commit();
   }
 
-  private void setActionBar() {
-    ActionBar actionBar = getSupportActionBar();
-    if (actionBar != null) {
-      actionBar.setDisplayHomeAsUpEnabled(true);
-    }
+  @Override protected Toolbar getToolbar() {
+    return toolbar;
   }
 
-  @Override public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case android.R.id.home:
-        finish();
-        break;
-    }
-    return super.onOptionsItemSelected(item);
+  @Override protected List<Presenter> getPresenter() {
+    return null;
   }
 }
