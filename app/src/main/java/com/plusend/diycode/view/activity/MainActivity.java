1cf5c12d8f49ec45b739eb0f9da47bb5668f1810
jian.zhang <jian.zhang@yulore.com>
2017-03-04 22:00:52 +0800

    change colorPrimaryDark

--- app/src/main/java/com/plusend/diycode/view/activity/MainActivity.java
+++ app/src/main/java/com/plusend/diycode/view/activity/MainActivity.java
@@ -60,7 +60,8 @@ public class MainActivity extends AppCompatActivity
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
-    StatusBarUtil.setColorForDrawerLayout(this, drawerLayout, 0xF0F0F0);
+    StatusBarUtil.setColorForDrawerLayout(this, drawerLayout,
+        getResources().getColor(R.color.colorPrimaryDark), 1);
 
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     toolbar.setLogo(R.mipmap.logo_actionbar);
