db6792846c6a7997a601ca2e5fb2c04d35c06fc6
jian.zhang <jian.zhang@yulore.com>
2017-04-04 15:32:10 +0800

    update dependencies

--- app/src/main/java/com/plusend/diycode/view/activity/ImageActivity.java
+++ app/src/main/java/com/plusend/diycode/view/activity/ImageActivity.java
@@ -12,52 +12,53 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
+import com.github.chrisbanes.photoview.PhotoView;
 import com.plusend.diycode.R;
 import com.plusend.diycode.model.base.BasePresenter;
 import java.util.List;
-import uk.co.senab.photoview.PhotoView;
 
 public class ImageActivity extends BaseActivity {
-  private static final String TAG = "ImageActivity";
-  public static final String URL = "url";
+    private static final String TAG = "ImageActivity";
+    public static final String URL = "url";
 
-  @BindView(R.id.toolbar) Toolbar toolbar;
-  @BindView(R.id.progress_bar) ProgressBar progressBar;
-  @BindView(R.id.image) PhotoView image;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.progress_bar) ProgressBar progressBar;
+    @BindView(R.id.image) PhotoView image;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    setContentView(R.layout.activity_image);
-    ButterKnife.bind(this);
-    super.onCreate(savedInstanceState);
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_image);
+        ButterKnife.bind(this);
+        super.onCreate(savedInstanceState);
 
-    String url = getIntent().getStringExtra(URL);
-    Log.d(TAG, "url: " + url);
+        String url = getIntent().getStringExtra(URL);
+        Log.d(TAG, "url: " + url);
 
-    Glide.with(this)
-        .load(url)
-        .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-        .listener(new RequestListener<String, GlideDrawable>() {
-          @Override
-          public boolean onException(Exception e, String model, Target<GlideDrawable> target,
-              boolean isFirstResource) {
-            progressBar.setVisibility(View.GONE);
-            return false;
-          }
+        Glide.with(this)
+            .load(url)
+            .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+            .listener(new RequestListener<String, GlideDrawable>() {
+                @Override
+                public boolean onException(Exception e, String model, Target<GlideDrawable> target,
+                    boolean isFirstResource) {
+                    progressBar.setVisibility(View.GONE);
+                    return false;
+                }
 
-          @Override public boolean onResourceReady(GlideDrawable resource, String model,
-              Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
-            progressBar.setVisibility(View.GONE);
-            return false;
-          }
-        })
-        .into(image);
-  }
+                @Override public boolean onResourceReady(GlideDrawable resource, String model,
+                    Target<GlideDrawable> target, boolean isFromMemoryCache,
+                    boolean isFirstResource) {
+                    progressBar.setVisibility(View.GONE);
+                    return false;
+                }
+            })
+            .into(image);
+    }
 
-  @Override protected Toolbar getToolbar() {
-    return toolbar;
-  }
+    @Override protected Toolbar getToolbar() {
+        return toolbar;
+    }
 
-  @Override protected List<BasePresenter> getPresenter() {
-    return null;
-  }
+    @Override protected List<BasePresenter> getPresenter() {
+        return null;
+    }
 }
